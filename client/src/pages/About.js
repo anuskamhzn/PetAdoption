525f000d8759416e5a98adb1a00b7bf77e1f3ece
Ronesh <nakarmi.ronesh@gmail.com>
2024-05-09 16:58:39 +0545

    About Section

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,33 +1,44 @@
 import React from "react";
-import Layout from "./../components/Layout/Layout";
 import { Link } from "react-router-dom";
+import bg from "../imag/Aboutus.jpg";
+import Layout from "./../components/Layout/Layout";
+
+import SubBanner from "./SubBanner";
+import Footer from "../components/Layout/Footer.js";
+import Top from "./AboutUs/Top";
+import Middle from "./AboutUs/Middle";
+import Choosing from "./ChoosingUs";
+// import Card from "./CaseStudy";
 
 const About = () => {
   return (
     <Layout>
-      <div className="row contactus ">
-        <div className="col-md-6 ">
-          <img
-            src="/images/about.jpg"
-            alt="about"
-            style={{ width: "100%" }}
-          />
-        </div>
-        <div className="col-md-4">
-        <h1 className="bg-dark p-2 text-white text-center">About US</h1>
-          <p className="text-justify mt-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
-            perferendis eius temporibus dicta blanditiis doloremque explicabo
-            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
-            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
-            commodi illum quidem neque tempora nam.
-          </p>
-          <Link to="/homepage" className="pnf-btn">
-          Go Back
-        </Link>
-        </div>
-      </div>
+      {/* Banner Section  */}
+      <section>
+        <SubBanner heading="About Us" subHeading="Home /About Us" img={bg} />
+      </section>
+
+      {/* Top Section  */}
+      <section>
+        <Top />
+      </section>
+      <section>
+        <Choosing />
+      </section>
+      {/* Middle Section  */}
+      <section>
+        <Middle />
+      </section>
+
+      {/* Bottom Section  */}
+      {/* <section>
+        <Card />
+      </section> */}
+
+      {/* Footer Section  */}
+      <section>
+        <Footer />
+      </section>
     </Layout>
   );
 };
